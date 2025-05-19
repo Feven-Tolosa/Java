4b317d41bbd4685650e2b7a5bce57736ec7ca6a2
Feven-Tolosa <tolosafeven0@gmail.com>
2025-05-19 22:51:43 +0300

    override existing values

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
     int x = 55;
-    int y = 10;
+    final int y = 10;
   
     public static void main(String[] args) {
       Main myObj = new Main();
-      Main myObj2 = new Main();
-      System.out.println(myObj2.x);
+      myObj.x = 50;
+      System.out.println(myObj.x);
     }
   }
   
