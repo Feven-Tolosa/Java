dd189adf1532e27ebf3f2a9c0385d42ba6bf4a31
Feven-Tolosa <tolosafeven0@gmail.com>
2025-05-19 23:34:22 +0300

    Static vs. Public

--- Main.java
+++ Main.java
@@ -1,11 +1,25 @@
 public class Main {
-    int x = 55;
-    final int y = 10;
+    int x = 5;
+    String fname = "John";
+    String lname = "Doe";
+    int age = 24;
   
     public static void main(String[] args) {
+    //   Main myObj1 = new Main();  // Object 1
+    //   Main myObj2 = new Main();  // Object 2
+    //   myObj2.x = 25;
+    //   System.out.println(myObj1.x);  // Outputs 5
+    //   System.out.println(myObj2.x);  // Outputs 25
       Main myObj = new Main();
-      myObj.x = 50;
-      System.out.println(myObj.x);
+      System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+      System.out.println("Age: " + myObj.age);
+      myMethod();
     }
+    static void myMethod() {
+        System.out.println("Hello World!");
+      }
   }
-  
+   
+
+// Public, which can only be accessed by objects
+// Static method, which means that it can be accessed without creating an object of the class, unlike public
