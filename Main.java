3035eef6a1809247c8673521431b2a64fcf97afc
Feven-Tolosa <tolosafeven0@gmail.com>
2025-05-19 22:30:49 +0300

    Multiple Classes

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
-    int x = 5;
+    int x = 55;
     int y = 10;
   
     public static void main(String[] args) {
       Main myObj = new Main();
       Main myObj2 = new Main();
-      System.out.println(myObj2.y);
+      System.out.println(myObj2.x);
     }
   }
   
