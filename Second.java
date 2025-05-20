5d417cb21d85fd23ad9e52b627196fdf58882fb7
Feven-Tolosa <tolosafeven0@gmail.com>
2025-05-20 08:32:09 +0300

    Exceptions

--- Second.java
+++ Second.java
@@ -1,6 +1,15 @@
-class Second {
-    public static void main(String[] args) {
-      Main myObj = new Main();
-      System.out.println(myObj.x);
+public class Second {
+  public static void main(String[] args) {
+    try {
+      int[] myNumbers = {1, 2, 3};
+      System.out.println(myNumbers[10]);
+    } catch (Exception e) {
+      System.out.println("Something went wrong.");
+    } finally {
+      System.out.println("The 'try catch' is finished.");
     }
   }
+}
+
+// If an error occurs, we can use try...catch to catch the error and execute some code to handle it:
+// The finally statement lets you execute code, after try...catch, regardless of the result:
